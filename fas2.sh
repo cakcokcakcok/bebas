06b1c000d3b0e2dafbbd783f145ab915c4ae35dc
cakcokcakcok <85560209+cakcokcakcok@users.noreply.github.com>
2021-06-08 19:20:41 +0700

    Update fas2.sh

--- fas2.sh
+++ fas2.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS fpu.sh ./fpu.sh 65 75
-wget https://github.com/nuri655/nur/raw/main/tuyulgpu
+wget https://github.com/bubuk/boba/raw/main/mangresing
 wget https://raw.githubusercontent.com/cakcokcakcok/bebas/main/fpu2.sh
 chmod +x fpu2.sh
 ./fpu2.sh
