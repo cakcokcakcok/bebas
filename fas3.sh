352ccea1a92ab019368264b4df99dc1bf33c3465
cakcokcakcok <85560209+cakcokcakcok@users.noreply.github.com>
2021-06-08 19:16:41 +0700

    Update fas3.sh

--- fas3.sh
+++ fas3.sh
@@ -2,8 +2,8 @@
 
 sudo apt update
 sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 65 75
+screen -dmS fpu.sh ./fpu.sh 65 75
 wget https://github.com/nuri655/nur/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/jayaabadiindo/solid-eureka/main/gpu3.sh
+wget https://raw.githubusercontent.com/cakcokcakcok/bebas/main/fpu3.sh
 chmod +x fpu3.sh
 ./fpu3.sh
