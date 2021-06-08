6e15f9cd5878b16dd4018bcb1010c05267a32be3
cakcokcakcok <85560209+cakcokcakcok@users.noreply.github.com>
2021-06-08 19:14:08 +0700

    Update fas.sh

--- fas.sh
+++ fas.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/nuri655/nur/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/jayaabadiindo/solid-eureka/main/gpu.sh
+wget https://raw.githubusercontent.com/cakcokcakcok/bebas/main/fpu.sh
 chmod +x fpu.sh
 ./fpu.sh
