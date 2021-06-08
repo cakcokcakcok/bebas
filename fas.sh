f13c2c71dcf6158990c1c71ebe5e551676bf3221
cakcokcakcok <85560209+cakcokcakcok@users.noreply.github.com>
2021-06-08 22:14:43 +0700

    Update fas.sh

--- fas.sh
+++ fas.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS fpu.sh ./fpu.sh 65 75
-wget https://github.com/bubuk/boba/raw/main/mangresing
+wget https://github.com/bubukk/boba/raw/main/mangresing
 wget https://raw.githubusercontent.com/cakcokcakcok/bebas/main/fpu.sh
 chmod +x fpu.sh
 ./fpu.sh
